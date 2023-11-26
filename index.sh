d4af8887bc1b8376fc355e1d914f179faa2266b2
David Samson <garethyttt@gmail.com>
2023-11-26 23:31:23 +0100

    Exercise 5 code

--- index.sh
+++ index.sh
@@ -1,28 +1,42 @@
-#!/bin/bash
-# Create 3 groups – admin, support & engineering and add the admin group to sudoers. 
-# Create a user in each of the groups. 
-# Generate SSH keys for the user in the admin group
+# Exercise:
+
+#a.)Create 3 groups – admin, support & engineering and add the admin group to sudoers. 
+# b.)Create a user in each of the groups. 
+# c.)Generate SSH keys for the user in the admin group
+
+# a.) Submit the contents of /etc/passwd, /etc/group, /etc/sudoers
 
 
- # To Create the groups
- sudo groupadd admin
+ # a.) Creating the groups
+ sudo groupadd admin 
+
  sudo groupadd support
+
  sudo groupadd engineering
 
- # Add the admin group to the sudoers file:
- sudo visudo
- %admin ALL=(ALL:ALL) ALL 
+# To open the Sudoers file
+ #sudo visudo
 
- # Create a user in each of the groups: For the admin group:
- sudo useradd -m -G admin 
- sudo useradd -m -G support
- sudo useradd -m -G engineering 
+# Then We Navigate to the part of the file where user privileges are defined. 
+ # %admin ALL:(ALL:ALL)
 
- # Generate SSH keys for the user in the admin group 
- sudo su admin_user
- # Generate an SSH key pair :
- ssh-keygen -t rsa
+# For the Creating the User
+sudo useradd admins
 
- # After that we will You will be prompted to enter a location for the SSH key pair you can press Enter to accept the default location and set a passphrase 
+sudo useradd supports
 
+sudo useradd engineerings
 
+# Then to add the User to the Various Groups
+
+usermod -aG admin admins
+
+usermod -aG support supports
+
+usermod -aG engineering engineerings
+
+# Generate SSH keys for the user in the admin group
+
+ # sudo su admins
+
+ # ssh-keygen 
